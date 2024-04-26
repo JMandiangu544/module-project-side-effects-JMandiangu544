0b54008904c39e68492ed5bc128f92739c872ff1
Joberthy Mandiangu <joberthymambu@gmail.com>
2024-04-25 23:02:50 -0500

    Updates

--- frontend/components/Comments/Comment.js
+++ frontend/components/Comments/Comment.js
@@ -6,7 +6,7 @@ const Comment = props => {
   const { comment } = props;
 
   return (
-    <div className='comment-text'>
+    <div  className='comment-text'>
       <span className='user'>{comment.username}: </span>
       <span className='comment'>{comment.text}</span>
     </div>
